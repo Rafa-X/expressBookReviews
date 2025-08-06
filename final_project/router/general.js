5bb3a41b4a6fb4be720da7b5b64b7e45df44b4b1
Rafa-X <RD.2425@protonmail.com>
2025-08-05 21:25:47 -0400

    .

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,8 +26,11 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    console.log(req.body.author);
+    const filtered = Object.entries(books)
+    .filter(([key,book]) => book.author === req.body.author)
+    .map(([key,book]) => ({ id: key, ...book}));
+    return res.status(200).send(JSON.stringify(filtered, null, 4));
 });
 
 // Get all books based on title
