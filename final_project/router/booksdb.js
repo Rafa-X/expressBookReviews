5bd25f2d5b16d6d443d96812104747395f296cc9
Rafa-X <RD.2425@protonmail.com>
2025-08-07 21:22:46 -0400

    .

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,12 +1,12 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {}, "isbn": "978-0-306-40615-7" },
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {1:"Good",2:"Must read"}, "isbn": "978-0-306-40615-7" },
       2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
       3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
       4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
       5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {1:"Good",2:"Must read"} },
       7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {1:"Good old",2:"Epoch book",3:"Must read"} },
       9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
       10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
 }
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,18 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if(username && password){
+        if(!isValid(username)){
+            users.push({"username":username, "password":password});
+            return res.status(200).json({message:"User registered."});
+        }else{
+            return res.status(404).json({message:"Error. User already registered."});
+        }
+    }
+    return res.status(404).json({message:"Error. Information missing."});
 });
 
 // Get the book list available in the shop
@@ -35,14 +45,20 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    console.log(req.body.title);
+    const filtered = Object.entries(books)
+    .filter(([key,book]) => book.title === req.body.title)
+    .map(([key,book]) => ({ id: key, ...book}));
+    return res.status(200).send(JSON.stringify(filtered, null, 4));
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    console.log(req.body.isbn);
+    const filtered = Object.entries(books)
+    .filter(([key,book]) => book.isbn == req.body.isbn)
+    .flatMap(([key,book]) => Object.entries(book.reviews));
+    return res.status(200).send(JSON.stringify(filtered, null, 4));
 });
 
 module.exports.general = public_users;
